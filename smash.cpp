a3bbfaa14b7b80fdb439de29cd31ceed493e7def
MichaelBun <micha3008@gmail.com>
2016-04-19 10:02:42 +0300

    modified:   smash.cpp

--- smash.cpp
+++ smash.cpp
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
 		fgets(lineSize, MAX_LINE_SIZE, stdin);
 		strcpy(cmdString, lineSize);    
 		printf("%s\n%s\n",lineSize,cmdString);
-		print("%d\n",strlen(cmdString));
+		printf("%d\n",strlen(cmdString));
 		cmdString[strlen(cmdString)-1]='\0'; //SEGMENTATION FAULT
 					// perform a complicated Command
 		printf("%s",cmdString);
